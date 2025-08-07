4ae2ba096fc2b08de9ecda879fb35653eb902ad3
Nikhilverma124 <mrverma124@gmail.com>
2025-08-07 15:45:11 +0530

    final update

--- script.js
+++ script.js
@@ -3,16 +3,64 @@ let compScore=0;
 
 let choices=document.querySelectorAll(".choice");
 
+let uScore=document.querySelector("#user-score");
+let cScore=document.querySelector("#comp-score");
+let msg=document.querySelector("#msg");
+
 const genCompChoice=()=>{
     // rock , paper ,scissor
     const option=["rock","paper","scissors"];
 
-    Math.floor(Math.random()*3);
+    const random=Math.floor(Math.random()*3);
+    return option[random];
+    
+}
+
+const drawGame=()=>{
+    console.log("game was draw");
+    msg.innerText="Game was Draw. Play Again";
+    msg.style.backgroundColor="black";
+}
+
+const showWinner=(userWin, userChoice, compChoice)=>{
+    if(userWin){
+        console.log("you win");
+        userScore++;
+        uScore.innerText=userScore;
+        msg.innerText=`You Win!.Your ${userChoice} beat ${compChoice} `;
+        msg.style.backgroundColor="green";
+    }else{
+        console.log("you lose !");
+        compScore++;
+        cScore.innerText=compScore;
+        msg.innerText=`You Lose!. ${compChoice} beat Your ${userChoice}`;
+        msg.style.backgroundColor="red";
+    }
 }
 
 const playGame=(userChoice)=>{
     console.log("userChoice =",userChoice);
 
+   const compChoice=genCompChoice();
+
+   console.log("compchoice",compChoice);
+
+   if(userChoice === compChoice){
+        drawGame();
+   }else{
+    let userWin=true;
+    if(userChoice==="rock"){
+
+        userWin=compChoice==="paper" ? false :true;
+
+    }else if(userChoice==="paper"){
+        userWin=compChoice==="rock" ?true : false;
+    }else{
+        userWin=compChoice==="rock" ? false :true;
+    }
+
+    showWinner(userWin, userChoice, compChoice);
+   }
 
 }
 
@@ -20,7 +68,7 @@ choices.forEach((choice)=>{
 
     choice.addEventListener("click",()=>{
         const userChoice=choice.getAttribute("id");
-        console.log("clicked",userChoice);
+        //console.log("clicked",userChoice);
         playGame(userChoice)
     });
 });
